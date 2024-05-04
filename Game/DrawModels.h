162030b3199afe7a0f141793296f1b4d02a4a455
spfDRN <loganov.ru@gmail.com>
2024-05-04 12:52:43 +0400

    spf

---
+++ Game/DrawModels.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <windows.h>
+
+void Robot(HDC hdc, int cx, int cy);
+void zayc(HDC hdc, int cx, int cy);
+void volk(HDC hdc, int cx, int cy);
+void money(HDC hdc, int cx, int cy);
+//я xочу питсы 
